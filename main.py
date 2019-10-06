c5a26bd42b458b29707d3826e92cda03edbb0598
Sanidhya Mangal <mangalsanidhya19@gmail.com>
2019-10-07 02:09:31 +0530

    added: prepared data to be converted into tf.data

--- main.py
+++ main.py
@@ -53,3 +53,58 @@ def create_dataset(path, num_examples=None):
 
     # return zipped word pairs
     return zip(*word_pairs)
+
+# function to get max length of a tensor
+def max_length(tensor):
+    return max(len(t) for t in tensor)
+
+# tokenize the language
+def tokenize(lang):
+
+    # create a language tokenizer
+    lang_tokenizer = tf.keras.preprocessing.text.Tokenizer(filters='')
+
+    # fit the text lang to this tokenizer
+    lang_tokenizer.fit_on_texts(lang)
+
+    # get the tensor sequences
+    tensor = lang_tokenizer.texts_to_sequences(lang)
+
+    # pad the tensor sequences
+    tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor, padding='post')
+
+    return tensor, lang_tokenizer
+
+
+# function to load the dataset 
+def load_dataset(path, num_examples=None):
+
+    # create dataset
+    inp_lang, targ_lang =  create_dataset(path, num_examples)
+
+    # tokenize input lang
+    input_tensor, input_lang_tokenizer = tokenize(inp_lang)
+
+    # tokenize target lang
+    target_tensor, target_lang_tokenizer = tokenize(targ_lang)
+
+    return input_tensor, target_tensor,  input_lang_tokenizer, target_lang_tokenizer
+
+
+# limit the size of training to 3000
+num_examples = 3000
+
+# load dataset
+input_tensor, target_tensor, input_lang, target_lang = load_dataset(path_to_file, num_examples)
+
+# calculate max length of input and target tensor
+max_input_tensor, max_target_tensor = max_length(input_tensor), max_length(target_tensor)
+
+# split the dataset
+input_tensor_train, input_tensor_val, target_tensor_train, target_tensor_val = train_test_split(input_tensor, target_tensor, test_size=0.2)
+
+# function to convert the tensor into text
+def convert(lang, tensor):
+    for t in tensor:
+        if t!=0:
+            print("%d -------> %s" % (t, lang.index_word[t]))
