ace90e89b1e400f0b09030e4351aeea450ee8010
Sanidhya Mangal <mangalsanidhya19@gmail.com>
2019-10-07 17:26:10 +0530

    added: decoder unit

--- main.py
+++ main.py
@@ -205,3 +205,51 @@ class BahdanauAttention(tf.keras.Model):
 
 # attention layer
 attention_layer = BahdanauAttention(10)
+
+# Decoder Part 
+# Decoder Class
+class Decoder(tf.keras.Model):
+    """ A decoder class for the NMT Model"""
+
+    # constructor
+    def __init__(self, vocab_size, embedding_dim, dec_units, batch_sz):
+        # super class constructor
+        super(Decoder, self).__init__()
+
+        # batch size 
+        self.batch_sz = batch_sz
+        # dec units
+        self.dec_units = dec_units
+
+        # embedding layer
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+
+        # gru class
+        self.gru = tf.keras.layers.GRU(self.dec_units, return_sequence=True, return_state=True, recurrent_initializer='glorot_uniform')
+
+        # a fc layer
+        self.fc = tf.keras.layers.Dense(vocab_size)
+
+        # attention class
+        self.attention = BahdanauAttention(self.dec_units)
+
+    # call method 
+    def call(self, x, hidden, enc_output):
+        context_vector, attention_weights = self.attention(hidden, enc_output)
+
+        # embedding layer
+        x = self.embedding(x)
+
+        # concat context_vector and x 
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+
+        # optput from gru
+        output, state = self.gru(x)
+
+        # reshape output
+        output = tf.reshape(output, (-1, output.shape[2]))
+
+        # get the final output
+        x = self.fc(output)
+
+        return x, state, attention_weights
